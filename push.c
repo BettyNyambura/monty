76564752fc61d45bb76048aed751c0a0d92dc668
Betty Waiyego <btynyambura20@gmail.com>
2024-09-26 17:10:57 +0300

    push.c

--- push.c
+++ push.c
@@ -7,7 +7,6 @@
 void _push(stack_t **stack, __attribute__ ((unused))unsigned int line_number)
 {
 	stack_t *top;
-	(void)line_number;
 
 	top = malloc(sizeof(stack_t));
 	if (top == NULL)
@@ -19,6 +18,7 @@ void _push(stack_t **stack, __attribute__ ((unused))unsigned int line_number)
 	top->n = var_global.push_arg;
 	top->next = *stack;
 	top->prev = NULL;
+	
 	if (*stack != NULL)
 		(*stack)->prev = top;
 	*stack = top;
@@ -70,7 +70,7 @@ void _pop(stack_t **stack, unsigned int line_number)
 {
 	stack_t *nodo = *stack;
 
-	if (stack == NULL || *stack == NULL)
+	if (*stack == NULL)
 	{
 		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
 		exit(EXIT_FAILURE);
